7400a23f044c7911702d3ad9a08675f4592115ab
Mohsen Mokhtari Sarab <mohsen.m.sarab@gmail.com>
2019-05-02 22:59:01 +0300

    front-end

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Todo</title>
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,9 +15,14 @@
       <div class="list">
         <h1 class="header">To do.</h1>
 
-        <ul>
-          <li><span class="item">Pick up shopping</span></li>
-          <li><span class="item done">Learn php</span></li>
+        <ul class="items">
+          <li>
+            <span class="item">Pick up shopping</span>
+            <a href="#" class="done-button">Mark as done</a>
+          </li>
+          <li>
+            <span class="item done">Learn php</span>
+          </li>
         </ul>
 
         <form class="item-add" action="add.php" method="post">
